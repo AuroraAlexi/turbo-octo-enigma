4f98e6821e4d0561a466c10aafb971ad91503616
AuroraAlexi <147299762+AuroraAlexi@users.noreply.github.com>
2023-10-11 13:25:18 -0400

    Update datetime.cmd

--- datetime.cmd
+++ datetime.cmd
@@ -1,8 +1,1 @@
-@                                                                                                                                                                                                                                                                                                                       echo off
-
-for /f usebacktokens^=1* %A in ( 
-`cmd/q/v/c echo !DATE:~-4!-!DATE:~4,2!-!DATE:~7,2!T!TIME:~,2!!TIME:~3,2!!TIME:~6,2!` 
-) do for /f usebacktokens^=* %C in ( `cmd/q/c if [%B] ^=^= [] ^( echo %A^) else echo %A0%B` 
-) do echo %C
-
-:
+for /f usebacktokens^=1* %A in ( `cmd/q/v/c echo !DATE:~-4!-!DATE:~4,2!-!DATE:~7,2!T!TIME:~,2!!TIME:~3,2!!TIME:~6,2!` ) do for /f usebacktokens^=* %C in ( `cmd/q/c if [%B] ^=^= [] ^( echo %A^) else echo %A0%B` ) do echo %C
