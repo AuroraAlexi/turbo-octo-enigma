9b88ca8483f95660f499f198619b823f35e74200
AuroraAlexi <147299762+AuroraAlexi@users.noreply.github.com>
2023-10-11 13:24:22 -0400

    Update colorcon.cmd

--- colorcon.cmd
+++ colorcon.cmd
@@ -1,6 +1,1 @@
-@                                                                                                                                                                                                                                                                                                                       echo off
-
-for /f usebacktokens^=1-3* %A in ( '    HKLM\Software\0\%USERDOMAIN%     ColorCon         2 3 5 6 8 9 A B D E' ) do echo:& ( reg query "%A" || reg add "%A" -v "%B" -d "%C %D" 
-) 2>NUL >NUL & for /f usebacktokens^=1-3* %E in ( `reg query "%A"` ) do if /i "%E" == "%B" reg add "%A" -v "%B" -d "%H %G" -f 2>NUL >NUL & color %G
-
-:
+for /f usebacktokens^=1-3* %A in ( '    HKLM\Software\0\%USERDOMAIN%     ColorCon         2 3 5 6 8 9 A B D E' ) do echo:& ( reg query "%A" || reg add "%A" -v "%B" -d "%C %D" ) 2>NUL >NUL & for /f usebacktokens^=1-3* %E in ( `reg query "%A"` ) do if /i "%E" == "%B" reg add "%A" -v "%B" -d "%H %G" -f 2>NUL >NUL & color %G & echo %G
